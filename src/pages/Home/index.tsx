e107d5d01058ce7651ce578560bcc2a585663b93
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-07-25 22:28:37 -0300

    feat: Implement Styles and Basic Page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./h.module.css"
+
+export default function Home() {
+  return (
+    <div className={styles.textContainer}>
+        <h2 className={styles.title}>This is the Home Page of the Library :D</h2>
+    </div>
+  )
+}
