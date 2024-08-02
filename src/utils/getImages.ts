4e95dc4a42fd5edfbf25fd634397fd2fb5f0a6e4
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-01 22:27:08 -0300

    feat: Configure Imports and GetCovers Method

---
+++ src/utils/getImages.ts
@@ -0,0 +1,13 @@
+import duna1 from "../assets/duna-1.jpg"
+import duna2 from "../assets/duna-2.jpg"
+import duna3 from "../assets/duna-3.jpg"
+import duna4 from "../assets/duna-4.jpg"
+import duna5 from "../assets/duna-5.jpg"
+import duna6 from "../assets/duna-6.jpg"
+
+const getImages = () => {
+    const bookImages = [duna1, duna2, duna3, duna4, duna5, duna6];
+    return bookImages;
+}
+
+export default getImages
