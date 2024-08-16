2f6f9a9efcda5d2f75eb9b1b08c02db398bfcef2
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-15 22:31:39 -0300

    feat: Implement info Arrays

--- src/utils/getImages.ts
+++ src/utils/getImages.ts
@@ -4,9 +4,42 @@ import duna3 from "../assets/duna-3.jpg"
 import duna4 from "../assets/duna-4.jpg"
 import duna5 from "../assets/duna-5.jpg"
 import duna6 from "../assets/duna-6.jpg"
+import spaceCurrents from "../assets/correntes_espaco.jpg"
+import foundation from "../assets/fundacao.jpg"
+import foundationAndEmpire from "../assets/fundacao_imperio.jpg"
+import secondFoundation from "../assets/fundacao_segunda.jpg"
+import foundationEdge from "../assets/fundacao_limites.jpg"
+import foundationAndEarth from "../assets/fundacao_terra.jpg"
+import foundationPrelude from "../assets/fundacao_preludio.jpg"
+import steelCaves from "../assets/cavernas_aco.jpg"
+import rockSky from '../assets/pedra_ceu.jpg'
+import starDust from "../assets/poeira_estrelas.jpg"
+import dawnRobots from "../assets/robos_alvorada.jpg"
+import empireRobots from "../assets/robos_imperio.jpg"
+import unveiledSun from "../assets/sol_desvelado.jpg"
 
 const getImages = () => {
-    const bookImages = [duna1, duna2, duna3, duna4, duna5, duna6];
+    const bookImages = [
+        duna1, 
+        duna2, 
+        duna3, 
+        duna4, 
+        duna5, 
+        duna6,
+        spaceCurrents,
+        foundationPrelude,
+        foundation,
+        foundationAndEmpire,
+        secondFoundation,
+        foundationEdge,
+        foundationAndEarth,
+        steelCaves,
+        rockSky,
+        starDust,
+        dawnRobots,
+        empireRobots,
+        unveiledSun
+    ];
     return bookImages;
 }
 
---
+++ src/utils/getInfo.ts
@@ -0,0 +1,50 @@
+export const getBookNames = () => {
+    const bookNames = [
+        "Dune", 
+        "Dune Messiah", 
+        "Children of Dune", 
+        "God Emperor of Dune", 
+        "Heretics of Dune", 
+        "Chapterhouse Dune",
+        "The Currents of Space",
+        "Foundation Prelude",
+        "Foundation",
+        "Foundation and Empire",
+        "Second Foundation",
+        "Foundation's Edge",
+        "Foundation and Earth",
+        "Steel Caves",
+        "Star Dust",
+        "Sky Stone",
+        "Dawn Robots",
+        "Empire Robots",
+        "Unveiled Son"
+    ]
+    return bookNames;
+}
+
+export const getAuthors = () => {
+    const bookAuthors = [
+        "Frank Herbert", 
+        "Frank Herbert", 
+        "Frank Herbert", 
+        "Frank Herbert", 
+        "Frank Herbert", 
+        "Frank Herbert",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov",
+        "Isaac Asimov"
+
+    ]
+    return bookAuthors;
+}
