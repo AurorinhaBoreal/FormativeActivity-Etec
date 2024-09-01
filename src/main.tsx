332e1f0e161c4bc0feddc99b9e43271e3ebd010d
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-01 10:36:53 -0300

    feat: Implement Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import '/src/styles/main.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '/src/styles/main.css'
-import Home from './pages/Home'
+import Routes from './routes/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home />
+    <Routes />
   </React.StrictMode>,
 )
