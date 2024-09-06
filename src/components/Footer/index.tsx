dca7e98737ba6d83ab91a4bfd3bb0bb0d2072f4b
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-09-06 01:15:48 -0300

    feat: Implement Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./footer.module.css"
+
+const Footer = () => {
+    
+    return (
+        <div className={styles.footerBody}>
+            <h4>Feito por Aurora Kruschewsky, 2024</h4>
+        </div>
+    )
+}
+
+export default Footer
