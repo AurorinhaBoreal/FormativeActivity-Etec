7b4684ab198453ead9626712a8a70cb71bad3202
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-11-15 15:42:14 -0300

    feat: Implement text when no book are rendered

--- src/components/CardBook/index.tsx
+++ src/components/CardBook/index.tsx
@@ -3,9 +3,11 @@ import getImages from "../../utils/getImages"
 import Button from "../Button";
 import { useEffect, useState } from "react";
 import Book from "../../types/Book";
+import Modal from "../Modal";
 
 const Index = () => {
     const [booksInfo, setBooksInfo] = useState<Book[]>([])
+    const [selectedBook, setSelectedBook] = useState<Book | null>(null)
 
     useEffect(() => {
         fetch("http://localhost:3000/listagemLivros", {
@@ -21,7 +23,6 @@ const Index = () => {
             return res.json();
         })
         .then((data) => {
-            console.log(data.data);
             setBooksInfo(data.data)
         })
         .catch((error) => {
@@ -33,21 +34,34 @@ const Index = () => {
 
     return (
         <>
-            {booksInfo.map((book, index) => (
-                <div key={index} className={styles.cardContainer}>
-                    <div className={styles.imgWrapper}>
-                        <img src={booksCover[index]} className={styles.bookCover} />
-                    </div>
-                    <hr className={styles.cardDivider}/>
-                    <div className={styles.textWrapper}>
-                        <h3 className={styles.name}>{book.nome_livro}</h3>
-                        <h4 className={styles.author}>{book.autor_livro}</h4>
-                    </div>
-                    {/* <div className={styles.btnWrapper}>
-                        <Button btnText="Book Info"/>
-                    </div> */}
+            {booksInfo.length <= 0 ? (
+                <div className={styles.noBooks}>
+                    <h3 className={styles.noBooksText}>
+                        We don't have registered books to show. If you want to visualize them, go to the register page.
+                    </h3>
                 </div>
-            ))}
+            ) : (
+                <>
+                    {booksInfo.map((book, index) => (
+                    <div key={index} className={styles.cardContainer}>
+                        <div className={styles.imgWrapper}>
+                            <img src={booksCover[index]} className={styles.bookCover} />
+                        </div>
+                        <hr className={styles.cardDivider}/>
+                        <div className={styles.textWrapper}>
+                            <h3 className={styles.name}>{book.nome_livro}</h3>
+                            <h4 className={styles.author}>{book.autor_livro}</h4>
+                        </div>
+                        <div className={styles.btnWrapper}>
+                            <Button
+                                btnText="Book Info" 
+                                afterClick={() => setSelectedBook(book)}
+                            />
+                            {selectedBook && <Modal bookInfo={selectedBook} setModalVisible={() => setSelectedBook(null)}/>}
+                        </div>
+                    </div>))}
+                </>
+            )}
         </>
     )
 };
